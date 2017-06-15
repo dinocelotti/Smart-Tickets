f7c132bcaecf182248a7ade1f7a6ed82ed8e822d
James Prado <jamesprado7@gmail.com>
2017-06-15 11:36:57 -0400

    Chore: configure react rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,12 +16,14 @@ module.exports = {
 	rules: {
 		'consistent-return': 2,
 		'linebreak-style': [2, 'unix'],
-		'no-case-declarations' : 0,
+		'no-case-declarations': 0,
 		'no-else-return': 1,
-		'semi': [1, 'always'],
-		'quotes': [1, 'single'],
+		semi: [1, 'never'],
+		quotes: [1, 'single'],
 		'space-unary-ops': 2,
 		'no-console': [1, { allow: ['warn', 'error', 'log'] }],
 		'no-unused-vars': 1,
+		'react/prop-types': [1],
+		'react/display-name': [1],
 	},
 }
