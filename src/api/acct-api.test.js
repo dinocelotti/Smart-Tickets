a88a1f100c975bdbd84265304c4457eeaf6fce16
henrynguyen5 <ngu.henry@hotmail.com>
2017-06-24 01:27:46 -0400

    Update acct-api test

--- src/api/acct-api.test.js
+++ src/api/acct-api.test.js
@@ -1,5 +1,12 @@
 /* eslint-env jest */
-import * as api from './acct-api';
+import * as api from './acct-api'
 it('returns all accounts', async () => {
-	await expect(api.getAcctsAndBals()).resolves.toHaveLength(10);
-});
+	await expect(api.getAcctsAndBals()).resolves.toEqual(
+		expect.arrayContaining([
+			expect.objectContaining({
+				addr: expect.any(String),
+				balance: expect.any(String)
+			})
+		])
+	)
+})
