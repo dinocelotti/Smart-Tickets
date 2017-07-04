f909c6d67defd7aa411ea33778b82a43d65b0df0
henrynguyen5 <ngu.henry@hotmail.com>
2017-07-04 16:21:57 -0400

    Refactor: tix-reducer to use object destructuring in parameters

--- src/reducers/tix-reducer.js
+++ src/reducers/tix-reducer.js
@@ -1,35 +1,35 @@
 import * as types from './../actions/action-types'
 import { combineReducers } from 'redux'
 
-const byId = (state = {}, action) => {
-	switch (action.type) {
+const byId = (state = {}, { type, payload: { tix } = {} }) => {
+	switch (type) {
 		case types.LOAD_TIX_SUCCESS:
 			return {
 				...state,
-				...action.tix.reduce((total, t) => {
+				...tix.reduce((total, t) => {
 					total[t.id] = t
 					return total
 				}, {})
 			}
 		case types.EVENT_PROJ_ADD_TIX:
-			return { ...state, [action.tix.id]: action.tix }
+			return { ...state, [tix.id]: tix }
 		case types.EVENT_PROJ_ADD_IPFS_DETAILS_TO_TIX:
-			return { ...state, [action.tix.id]: { ...state[action.tix.id], ipfsHash: action.tix.ipfsHash } }
+			return { ...state, [tix.id]: { ...state[tix.id], ipfsHash: tix.ipfsHash } }
 		case types.EVENT_PROJ_SET_TIX_PRICE:
-			return { ...state, [action.tix.id]: { ...state[action.tix.id], price: action.tix.price } }
+			return { ...state, [tix.id]: { ...state[tix.id], price: tix.price } }
 		case types.EVENT_PROJ_SET_TIX_QUANTITY:
-			return { ...state, [action.tix.id]: { ...state[action.tix.id], quantity: action.tix.quantity } }
+			return { ...state, [tix.id]: { ...state[tix.id], quantity: tix.quantity } }
 		default:
 			return state
 	}
 }
-const ids = (state = [], action) => {
-	switch (action.type) {
+const ids = (state = [], { type, payload: { tix } = {} }) => {
+	switch (type) {
 		case types.LOAD_TIX_SUCCESS:
-			return [...new Set([...state, ...action.tix.map(({ id }) => id)])]
+			return [...new Set([...state, ...tix.map(({ id }) => id)])]
 
 		case types.EVENT_PROJ_ADD_TIX:
-			return [...new Set([...state, action.tix.id])]
+			return [...new Set([...state, tix.id])]
 
 		default:
 			return state
