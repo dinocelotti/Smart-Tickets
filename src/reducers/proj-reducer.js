0c04d158335c81a0ef707e0862268ba538362d3c
henrynguyen5 <ngu.henry@hotmail.com>
2017-07-04 09:59:17 -0400

    Refactor: proj-reducer to use object destructoring in function parameters

--- src/reducers/proj-reducer.js
+++ src/reducers/proj-reducer.js
@@ -1,40 +1,40 @@
 import * as types from './../actions/action-types'
 import { combineReducers } from 'redux'
 
-const byId = (state = {}, action) => {
-	switch (action.type) {
+const byId = (state = {}, { type, payload: { projs, proj, tix, distribs, distribId } = {} }) => {
+	switch (type) {
 		case types.LOAD_PROJS_SUCCESS:
 			return {
 				...state,
-				...action.projs.reduce((obj, proj) => {
+				...projs.reduce((obj, proj) => {
 					obj[proj.addr] = proj
 					return obj
 				}, {})
 			}
 		case types.EVENT_PROJ_CREATED:
-			return { ...state, [action.proj.addr]: action.proj }
+			return { ...state, [proj.addr]: proj }
 
 		case types.EVENT_PROJ_FINISH_STAGING:
-			return { ...state, [action.proj.addr]: { ...state[action.proj.addr], state: 'Private Funding' } }
+			return { ...state, [proj.addr]: { ...state[proj.addr], state: 'Private Funding' } }
 		case types.EVENT_PROJ_START_PUBLIC_FUNDING:
-			return { ...state, [action.proj.addr]: { ...state[action.proj.addr], state: 'Public Funding' } }
+			return { ...state, [proj.addr]: { ...state[proj.addr], state: 'Public Funding' } }
 
 		case types.LOAD_TIX_SUCCESS:
-			return { ...state, [action.proj.addr]: { ...state[action.proj.addr], tix: action.tix } }
+			return { ...state, [proj.addr]: { ...state[proj.addr], tix } }
 
 		case types.EVENT_PROJ_ADD_TIX:
 			return {
 				...state,
-				[action.proj.addr]: { ...state[action.proj.addr], tix: [...state[action.proj.addr].tix, action.tix] }
+				[proj.addr]: { ...state[proj.addr], tix: [...state[proj.addr].tix, tix] }
 			}
 		case types.LOAD_DISTRIBS_SUCCESS:
-			return { ...state, [action.proj.addr]: { ...state[action.proj.addr], distribs: action.distribs } }
+			return { ...state, [proj.addr]: { ...state[proj.addr], distribs } }
 		case types.EVENT_PROJ_ADD_DISTRIB:
 			return {
 				...state,
-				[action.proj.addr]: {
-					...state[action.proj.addr],
-					distribs: [...state[action.proj.addr].distribs, action.distrib]
+				[proj.addr]: {
+					...state[proj.addr],
+					distribs: [...state[proj.addr].distribs, distribId]
 				}
 			}
 		//TODO: implement these
@@ -49,17 +49,18 @@ const byId = (state = {}, action) => {
 			return state
 	}
 }
-const ids = (state = [], action) => {
-	switch (action.type) {
+const ids = (state = [], { type, payload: { projs, proj } = {} }) => {
+	switch (type) {
 		case types.LOAD_PROJS_SUCCESS:
-			return [...new Set([...state, ...action.projs.map(({ id }) => id)])]
-
+			return [...new Set([...state, ...projs.map(({ addr }) => addr)])]
+		case types.EVENT_PROJ_CREATED:
+			return [...new Set([...state, proj.addr])]
 		default:
 			return state
 	}
 }
-const projResolver = (state = { deployed: false }, action) => {
-	switch (action.type) {
+const projResolver = (state = { deployed: false }, { type }) => {
+	switch (type) {
 		case types.PROJ_RESOLVER_DEPLOYED_SUCCESS:
 			return { ...state, deployed: true }
 
