2f68dc46fc22b2d85e13e9205e5955917613f517
henrynguyen5 <ngu.henry@hotmail.com>
2017-07-04 16:22:04 -0400

    Test: include basic tests for tix-reducer

--- src/reducers/tix-reducer.test.js
+++ src/reducers/tix-reducer.test.js
@@ -9,9 +9,71 @@ describe('tix-reducer', () => {
 			ids: []
 		})
 	})
-	it(`should handle ${types.EVENT_PROJ_ADD_TIX}`, () => {})
-	it(`should handle ${types.EVENT_PROJ_ADD_IPFS_DETAILS_TO_TIX}`, () => {})
-	it(`should handle ${types.EVENT_PROJ_SET_TIX_PRICE}`, () => {})
-	it(`should handle ${types.EVENT_PROJ_SET_TIX_QUANTITY}`, () => {})
-	it(`should handle ${types.LOAD_TIX_SUCCESS}`, () => {})
+	let state = {
+		byId: { TIXID00: { id: 'TIXID00' } },
+		ids: ['TIXID00']
+	}
+	it(`should handle ${types.EVENT_PROJ_ADD_TIX}`, () => {
+		expect(reducer(undefined, { type: types.EVENT_PROJ_ADD_TIX, payload: { tix: { id: 'TIXID00' } } })).toEqual(state)
+	})
+	let state2 = {
+		...state,
+		byId: { ...state.byId, [state.ids[0]]: { ...state.byId[state.ids[0]], ipfsHash: 'IPFSHASH00' } }
+	}
+	it(`should handle ${types.EVENT_PROJ_ADD_IPFS_DETAILS_TO_TIX}`, () => {
+		expect(
+			reducer(state, {
+				type: types.EVENT_PROJ_ADD_IPFS_DETAILS_TO_TIX,
+				payload: { tix: { ipfsHash: 'IPFSHASH00', id: state.ids[0] } }
+			})
+		).toEqual(state2)
+	})
+	let state3 = {
+		...state2,
+		byId: { ...state2.byId, [state2.ids[0]]: { ...state2.byId[state2.ids[0]], price: '1000' } }
+	}
+	it(`should handle ${types.EVENT_PROJ_SET_TIX_PRICE}`, () => {
+		expect(
+			reducer(state2, {
+				type: types.EVENT_PROJ_SET_TIX_PRICE,
+				payload: { tix: { price: '1000', id: state2.ids[0] } }
+			})
+		).toEqual(state3)
+	})
+	let state4 = {
+		...state3,
+		byId: { ...state3.byId, [state3.ids[0]]: { ...state3.byId[state3.ids[0]], quantity: '50' } }
+	}
+	it(`should handle ${types.EVENT_PROJ_SET_TIX_QUANTITY}`, () => {
+		expect(
+			reducer(state3, {
+				type: types.EVENT_PROJ_SET_TIX_QUANTITY,
+				payload: { tix: { quantity: '50', id: state3.ids[0] } }
+			})
+		).toEqual(state4)
+	})
+	it(`should handle ${types.LOAD_TIX_SUCCESS}`, () => {
+		expect(
+			reducer(
+				{
+					byId: { TIXID00: { id: 'TIXID00' } },
+					ids: ['TIXID00']
+				},
+				{ type: types.LOAD_TIX_SUCCESS, payload: { tix: [{ id: 'TIXID00' }, { id: 'TIXID01' }, { id: 'TIXID02' }] } }
+			)
+		).toEqual({
+			byId: {
+				TIXID00: {
+					id: 'TIXID00'
+				},
+				TIXID01: {
+					id: 'TIXID01'
+				},
+				TIXID02: {
+					id: 'TIXID02'
+				}
+			},
+			ids: ['TIXID00', 'TIXID01', 'TIXID02']
+		})
+	})
 })
