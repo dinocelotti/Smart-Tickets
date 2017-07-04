4ccba25ca2e0d896ac6d9bec3f9a5847e9fb3ca3
James Prado <jamesprado7@gmail.com>
2017-07-04 17:26:56 -0400

    Refactor: acc-tables-cont

--- src/containers/acct-tables-cont.jsx
+++ src/containers/acct-tables-cont.jsx
@@ -3,11 +3,15 @@ import React from 'react'
 import { connect } from 'react-redux'
 import * as acctApi from '../api/acct-api'
 import accTypes from '../prop-types/accts'
+import store from '../store'
+import { getAcctsSuccess } from '../actions/acct-actions'
 
 class AcctTableEthCont extends React.Component {
 	componentDidMount() {
 		//get acct addrs
-		acctApi.getAcctsAndBals()
+		acctApi
+			.getAcctsAndBals()
+			.then(accts => store.dispatch(getAcctsSuccess(accts)))
 	}
 	render() {
 		return (
@@ -21,14 +25,16 @@ class AcctTableEthCont extends React.Component {
 
 AcctTableEthCont.propTypes = {
 	accts: accTypes.accts,
-	acctsByAddr: accTypes.acctsByAddr,
+	acctsByAddr: accTypes.acctsByAddr
 }
 
 function mapEthStateToProps(store) {
 	return {
 		acctsByAddr: store.acctState.acctsByAddr,
-		accts: store.acctState.accts,
+		accts: store.acctState.accts
 	}
 }
 
-export const EthTable = connect(mapEthStateToProps)(AcctTableEthCont)
+export const EthTable = connect(mapEthStateToProps, { getAcctsSuccess })(
+	AcctTableEthCont
+)
