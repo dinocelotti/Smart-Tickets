9aa818eca93e08c17198ec025b9315780fb1e115
henrynguyen5 <ngu.henry@hotmail.com>
2017-07-06 18:19:44 -0400

    Test: add redux tools extension

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware } from 'redux'
 import reducers from './reducers'
+import { devToolsEnhancer } from 'redux-devtools-extension'
+/*
 const logger = store => next => action => {
 	console.log('dispatching', action)
 	let result = next(action)
 	console.log('next state', store.getState())
 	return result
-}
+}*/
 
-const store = createStore(reducers, applyMiddleware(logger))
+const store = createStore(reducers, devToolsEnhancer())
 export default store
