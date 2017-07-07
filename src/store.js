f8f3b2465c8f367db3a039c7c08bc52d8ee8ee06
henrynguyen5 <ngu.henry@hotmail.com>
2017-07-06 20:15:10 -0400

    Update: package.json

--- src/store.js
+++ src/store.js
@@ -1,13 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import reducers from './reducers'
 import { devToolsEnhancer } from 'redux-devtools-extension'
-/*
-const logger = store => next => action => {
-	console.log('dispatching', action)
-	let result = next(action)
-	console.log('next state', store.getState())
-	return result
-}*/
 
 const store = createStore(reducers, devToolsEnhancer())
 export default store
