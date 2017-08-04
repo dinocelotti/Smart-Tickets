e8561ef3c086ed3224fceeec74ffee8f9c193812
henrynguyen5 <ngu.henry@hotmail.com>
2017-08-04 18:06:16 -0400

    Setup smoke testing for project.sol contract

--- test/project.test.js
+++ test/project.test.js
@@ -1,19 +1,60 @@
 /* eslint-env jest*/
 //eslint-disable-next-line
-const Web3 = require('web3')
-const contract = require('truffle-contract')
 import * as deployment from '../scripts/testHelper'
 import accApi from '../src/api/account-api'
 import EthApi from '../src/api/eth-api'
 import projectApi from '../src/api/project-api'
-const Project = require('../build/contracts/Project.json')
+import utils from '../src/api/api-helpers'
+import prettyjson from 'prettyjson'
 
-const provider = new Web3.providers.HttpProvider('http://localhost:8545')
-const web3 = new Web3(provider)
+//eslint-disable-next-line
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000
 const ethApi = new EthApi()
-let project
-let projectResolver
-let accounts
+let project,
+	projectInstance,
+	projectResolver,
+	accounts,
+	promoterInstance,
+	distributorInstance
+
+const ticketsToAdd = []
+const printLogsFromEvent = ({ logs }) =>
+	logs.forEach(log => console.log(prettyjson.render(utils.normalizeArgs(log))))
+const sampleProjectGen = (function* sampleProjectGen() {
+	let index = 0
+	const projectName = num => `Sample Project ${num}`
+	const randomNumGen = seed => () => Math.floor(Math.random() * seed + 100)
+	const consumerMaxTickets = randomNumGen(5)
+	//eslint-disable-next-line
+	while (true) {
+		yield {
+			projectName: projectName(index),
+			totalTickets: 100,
+			consumerMaxTickets: consumerMaxTickets(),
+			promoterAddress: ''
+		}
+
+		index++
+	}
+})()
+const sampleTicketGen = (function* sampleTicketGen() {
+	let index = 0
+	const ticketType = num => `TicketType${num}`
+	const randomNumGen = seed => () => Math.floor(Math.random() * seed + 2)
+	const ticketPrice = randomNumGen(50)
+	//eslint-disable-next-line
+	while (true) {
+		yield {
+			ticketType: ticketType(index),
+			ticketPrice: ticketPrice(),
+			ticketQuantity: 100
+		}
+		index++
+	}
+})()
+for (let index = 0; index < 3; index++) {
+	ticketsToAdd.push(sampleTicketGen.next().value)
+}
 beforeAll(async () => {
 	try {
 		await deployment.init()
@@ -35,25 +76,84 @@ it('should add a bunch of projects to the first account', async () => {
 })
 
 describe('Project', () => {
-	it('should instantiate the project', async done => {
-		await projectApi.createProject({
-			projectName: 'myCoolProject',
-			consumerMaxTickets: 2,
-			promoterAddress: accounts[0],
-			totalTickets: 100
-		})
+	it('should instantiate the project and store its instance', async done => {
+		const sampleProject = sampleProjectGen.next().value
+		sampleProject.promoterAddress = accounts[0]
+		await projectApi.createProject(sampleProject)
 		const event = projectResolver.AddProject(
 			{},
 			{ fromBlock: 0, toBlock: 'pending' }
 		)
+
 		//find the project address
 		event.watch(async (error, log) => {
 			//watch the project and find it
-			const p = await project.at(log.args._project)
-			p.allEvents({ fromBlock: 0, toBlock: 'pending' }, (err, _log) => {
-				console.log(_log)
-				done()
-			})
+			projectInstance = await project.at(log.args._project)
+			projectInstance.allEvents(
+				{ fromBlock: 0, toBlock: 'pending' },
+				(err, _log) => {
+					console.log(utils.normalizeArgs(_log))
+					event.stopWatching()
+
+					done()
+				}
+			)
 		})
 	})
+	it('should add 3 types of tickets and their values', async done => {
+		try {
+			promoterInstance = new projectApi.Promoter({
+				promoter: accounts[0],
+				address: projectInstance.address
+			})
+			await promoterInstance.init()
+			//take up all the tickets instead
+			const res = await promoterInstance.handleTicketForm(ticketsToAdd[0])
+			printLogsFromEvent(res)
+		} catch (e) {
+			console.error(e)
+		}
+		done()
+	})
+	it('should add a distributor and assign it to the first ticket', async () => {
+		const distributorToAdd = {
+			distributor: accounts[1],
+			ticketType: ticketsToAdd[0].ticketType,
+			distributorAllottedQuantity: ticketsToAdd[0].ticketQuantity,
+			promoterFee: 10
+		}
+		const results = []
+		results.push(await promoterInstance.addDistributor(accounts[1]))
+		results.push(
+			await promoterInstance.setDistributorAllottedQuantity(distributorToAdd)
+		)
+		results.push(await promoterInstance.setDistributorFee(distributorToAdd))
+		results.forEach(event => printLogsFromEvent(event))
+	})
+	it('should let the distributor set the markup on the tickets to 10%', async () => {
+		try {
+			distributorInstance = new projectApi.Buyer({
+				buyerAddress: accounts[1],
+				isDistributor: true,
+				projectAddress: projectInstance.address
+			})
+			await distributorInstance.init()
+			const res = await distributorInstance.setMarkup({
+				markup: 10,
+				ticketType: ticketsToAdd[0].ticketType
+			})
+			printLogsFromEvent(res)
+		} catch (e) {
+			console.error(e)
+		}
+	})
+
+	it('should move the project to the next stage', async () => {
+		const res = await promoterInstance.finishStaging()
+		printLogsFromEvent(res)
+	})
+	it('should let the distributor buy all of the tickets', async () => {})
+})
+afterAll(async () => {
+	await deployment.end()
 })
